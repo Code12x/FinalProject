7f53914db55b492031742960d0a25f8a809a83c1
anne <athn221@gmail.com>
2023-11-15 10:54:28 -0500

    barebones homepage

---
+++ resources/views/Shared/homepage.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shady Oaks Retirement Village
+    </title>
+</head>
+<body>
+    <header>
+
+    </header>
+
+
+
+    <div class="">
+        <a href="#">Log In</a>
+        <a href="#">Sign Up</a>
+    </div>
+
+
+
+    <footer>
+
+    </footer>
+</body>
+</html>

de44f24e6182315774f9b0cdbd6e00e8e147d522
TrevorKimble <tvkimble04@gmail.com>
2023-11-16 09:03:51 -0500

    added html forms for login and register

--- resources/views/Authentication/Register.blade.php
+++ resources/views/Authentication/Register.blade.php
@@ -6,6 +6,35 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="" method="post">
+        <label for="Email">Email:</label><br>
+        <select id="dropdown" name="dropdown">
+            <option value="admin">Admin</option>
+            <option value="suprivisor">Suprivisor</option>
+            <option value="doctor">Doctor</option>
+            <option value="caregiver">Caregiver</option>
+            <option value="patient">Patinet</option>
+            <option value="familymember">Family Member</option>
+        </select>
+        <br>
+        <label for="firstname">First name:</label><br>
+        <input type="text" id = "firstname" name = "First Name">
+        <br>
+        <label for="lastname">Last Name:</label><br>
+        <input type="text" id = "lastname" name = "Last Name">
+        <br>
+        <label for="email">Email:</label><br>
+        <input type="email" id = "email" name = "Email">
+        <br>
+        <label for="phonenumber">:</label><br>
+        <input type="tel" id = "phonenumber" name = "Phone Number" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}">
+        <br>
+        <label for="">:</label><br>
+        <input type="text" id = "" name = "">
+        <br>
+        <label for="">:</label><br>
+        <input type="text" id = "" name = "">
+        
+    </form>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\AuthenticationController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Log in and Registration 
+Route::get('Authentication/Login', function(){
+    return view('Authentication\Login');
+});
+
+Route::get('Authentication/Register', function(){
+    return view('Authentication\Register');
 });

c260b51978e0e03f1701542feb966a8f89e85636
anne <athn221@gmail.com>
2023-11-16 10:20:16 -0500

    skeleton for form to access patient info

---
+++ resources/views/Shared/familyhomepage.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Family Member Home Page</title>
+</head>
+<body>
+    <header>
+
+    </header>
+
+    <div class="">
+
+        <form action="">
+            <label for="">Date</label>
+            <input type="date" name="date">
+
+            <label for="">Patient's Family Code</label>
+            <input type="text" name="familyCode">
+
+            <label for="">Patient ID</label>
+            <input type="number" name="patientId">
+        </form>
+
+    </div>
+
+    <footer>
+
+    </footer>
+
+</body>
+</html>

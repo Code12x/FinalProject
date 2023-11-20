c8b730e9ecca0e3f1a921b787629a21275349989
TrevorKimble <tvkimble04@gmail.com>
2023-11-20 11:29:11 -0500

    fixed access level

--- resources/views/Admin/create_role.blade.php
+++ resources/views/Admin/create_role.blade.php
@@ -14,12 +14,12 @@
         <br>
         <label for="AccessLevel">Access Level:</label><br>
         <select id="AccessLevel" name="intAccessLevel">
-            <option value="0">1</option>
-            <option value="1">2</option>
-            <option value="2">3</option>
-            <option value="3">4</option>
-            <option value="4">5</option>
-            <option value="5">6</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
         </select>
         <br>
         <input type="submit" value="Submit">

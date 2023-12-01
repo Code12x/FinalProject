65da9f4f5c92315e77494714e8d1f27ebf6b125a
anne <athn221@gmail.com>
2023-12-01 09:31:36 -0500

    ok

--- app/Models/Appointments.php
+++ app/Models/Appointments.php
@@ -11,11 +11,8 @@ class Appointments extends Model
     use HasFactory;
     
     protected $table = 'tblAppointments';
-<<<<<<< HEAD:app/Models/Appointments.php
-=======
 
     protected $primaryKey = 'intAppointmentId';
->>>>>>> 4f7f6d79d8efa14c4cd00559a98afd7526c8cec3:app/Models/Appointment.php
 
     protected $fillable = ['intAppointmentId',
                             'intPatientId',

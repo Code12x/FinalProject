cc4ee8ade5d88b420958f5df85aeb7818848fd00
anne <athn221@gmail.com>
2023-12-08 10:16:24 -0500

    login css setup

--- resources/views/Authentication/Login.blade.php
+++ resources/views/Authentication/Login.blade.php
@@ -4,16 +4,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <style>
+        * {
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+
+        form {
+            /* background-color: #604324; */
+            height: 400px;
+            width: 300px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+
+        body {
+            background-color:#FFE8C5;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+    </style>
+
 </head>
 <body>
-    <form action="" method="post">
-        <label for="Email">Email:</label><br>
-        <input type="email" id = "email" name = "Email"> <br>
-        <label for="Password">Password:</label><br>
-        <input type="password" id = "password" name = "Password">
-        <br>
-        <input type="submit" value="Submit">
-    </form>
+    <div class="formDiv">
+        <form action="" method="post">
+            <label for="Email">Email:</label><br>
+            <input type="email" id = "email" name = "Email"> <br>
+            <label for="Password">Password:</label><br>
+            <input type="password" id = "password" name = "Password">
+            <br>
+            <input type="submit" value="Submit">
+        </form>
+    </div>
 
     @if(isset($message))
         <script>

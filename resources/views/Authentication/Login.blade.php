ecba233c5d64a1636e901d20693bb8c14d55c77e
anne <athn221@gmail.com>
2023-12-08 11:20:45 -0500

    css'd login/registration page

--- resources/views/Authentication/Login.blade.php
+++ resources/views/Authentication/Login.blade.php
@@ -13,17 +13,51 @@
         }
 
         form {
-            /* background-color: #604324; */
-            height: 400px;
+            background-color: rgba(255, 255, 255, .95);
+            height: 300px;
             width: 300px;
             display: flex;
             flex-direction: column;
             justify-content: center;
             align-items: center;
+            border-radius: 20px;
+        }
+
+        input {
+            width: 200px;
+            height: 25px;
+            border-radius: 20px;
+            border-width: 1px;
+        }
+
+        #submitButton {
+            background-color: #3B2712;
+            /* background-color: rgba(165, 129, 91, 1); */
+            border-style: none;
+            cursor: pointer;
+            border-radius: 20px;
+            height: 35px;
+            color: white;
+        }
+
+        .divName {
+            height: 100vh;
+            background-color: rgba(165, 129, 91, .9);
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            margin-left: 20px;
+            padding: 50px;
+            color: white;
+            
         }
 
         body {
-            background-color:#FFE8C5;
+            height: 100vh;
+            background-image: url('images/loginRegisterBackground.jpeg');
+            background-size: cover;
+            background-color: #FAE7C7;
             display: flex;
             justify-content: center;
             align-items: center;
@@ -33,15 +67,27 @@
 
 </head>
 <body>
-    <div class="formDiv">
+    <div class="divForm">
         <form action="" method="post">
             <label for="Email">Email:</label><br>
             <input type="email" id = "email" name = "Email"> <br>
             <label for="Password">Password:</label><br>
             <input type="password" id = "password" name = "Password">
             <br>
-            <input type="submit" value="Submit">
+            <input type="submit" value="Log in" id="submitButton">
         </form>
+    </div>
+
+    <div class="divName">
+        <h1>
+            Shady Oaks
+        </h1>
+        <h1>
+            Retirement Village
+        </h1>
+        <h1>
+            Login Page
+        </h1>
     </div>
 
     @if(isset($message))
--- resources/views/Authentication/Register.blade.php
+++ resources/views/Authentication/Register.blade.php
@@ -8,16 +8,90 @@
         .hidden {
             display: none;
         }
+
+        * {
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+
+        form {
+            background-color: rgba(255, 255, 255, 0.95);
+            height: fit-content;
+            width: 300px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            border-radius: 20px;
+            padding-top: 15px;
+            padding-bottom: 15px;
+        }
+
+        .userTypeDropdown {
+            display: flex;
+        }
+
+        .dropdownLabel {
+            padding-right: 10px;
+        }
+
+        input {
+            width: 200px;
+            height: 25px;
+            border-radius: 20px;
+            border-width: 1px;
+        }
+
+        #submitButton {
+            background-color: #3B2712;
+            /* background-color: rgba(165, 129, 91, 1); */
+            border-style: none;
+            cursor: pointer;
+            border-radius: 20px;
+            height: 35px;
+            color: white;
+        }
+
+        .divSubmit {
+            padding-top: 25px;
+        }
+
+        .divName {
+            height: 100vh;
+            background-color: rgba(165, 129, 91, .9);
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            margin-left: 20px;
+            padding: 50px;
+            color: white;
+            
+        }
+
+        body {
+            height: 100vh;
+            background-image: url('images/loginRegisterBackground.jpeg');
+            background-size: cover;
+            background-color: #FAE7C7;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
     </style>
 </head>
 <body>
     <form action="" method="post" onchange="showInputs()">
-        <label for="dropdown">Email:</label><br>
-        <select id="dropdown" name="intRoleId">
-            @foreach ($roles as $role)
-            <option value="{{ $role->intRoleId }}">{{ $role->strName }}</option>
-            @endforeach
-        </select>
+        <div class="userTypeDropdown">
+            <label for="dropdown" class="dropdownLabel">User type </label><br>
+            <select id="dropdown" name="intRoleId">
+                @foreach ($roles as $role)
+                <option value="{{ $role->intRoleId }}">{{ $role->strName }}</option>
+                @endforeach
+            </select>
+        </div>
         <br>
         <label for="firstname">First name:</label><br>
         <input type="text" id = "firstname" name = "strFirstName">
@@ -48,9 +122,26 @@
             <input type="text" id="relationemergancycontact" name="strEmergencyContactRelation">
         </div>
 
-        <input type="submit" value="Submit">
-        
+        <div class="divSubmit">
+            <input type="submit" value="Register" id="submitButton">
+        </div>
+
+
+
     </form>
+
+    <div class="divName">
+        <h1>
+            Shady Oaks
+        </h1>
+        <h1>
+            Retirement Village
+        </h1>
+        <h1>
+            Registration Page
+        </h1>
+    </div>
+
 </body>
 
 <script>

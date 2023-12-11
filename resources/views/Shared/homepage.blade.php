9e00781674bc23828d2d89e7a1b8926f4a654906
anne <athn221@gmail.com>
2023-12-11 09:26:16 -0500

    small changes to css file

--- resources/views/Shared/homepage.blade.php
+++ resources/views/Shared/homepage.blade.php
@@ -5,7 +5,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shady Oaks Retirement Village
     </title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="">
+
+<style>
+
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+  /* header {
+
+  } */
+
+#navigationBar {
+  display: flex;
+  justify-content: space-between;
+  margin-left: 200px;
+  margin-right: 200px;
+}
+
+#navigationBar a:link, #navigationBar a:visited, #navigationBar a:active {
+  text-decoration: none;
+  color: black;
+}
+
+#navigationBar a:hover {
+  cursor: pointer;
+}
+
+#logInSignUp a:link, #logInSignUp a:visited, #logInSignUp a:active {
+  text-decoration: none;
+  color: black;
+}
+
+#logInSignUp a:hover {
+  cursor: pointer;
+}
+
+</style>
+    
 </head>
 <body>
     <header>

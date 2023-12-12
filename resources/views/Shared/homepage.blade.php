71a8b25d0e6861133c902b332cda61f6533877b4
Justin <jdkoch2855@gmail.com>
2023-12-12 10:39:45 -0500

    merge fixes

--- resources/views/Shared/homepage.blade.php
+++ resources/views/Shared/homepage.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!--<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -166,7 +166,6 @@ footer {
   </div>
 
     <div id="logInSignUpLinks">
-      <!--
       <a class="authLink" href="/login">LOGIN</a>
       <a class="authLink" href="/register">REGISTER</a>
     </div>
@@ -208,7 +207,6 @@ footer {
         Many styles of dancing
       </p>
     </div>
-  -->
       <div id="logInSignUpLinksBoxes">
         <a href="/login" id="logInSignUpLinksBoxes2">Login</a>
       </div>
@@ -250,5 +248,104 @@ footer {
     </div>
 
   </footer>
+</body>
+</html>
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shady Oaks Retirement Village
+    </title>
+    <link rel="stylesheet" href="">
+
+<style>
+
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+  /* header {
+
+  } */
+
+#navigationBar {
+  display: flex;
+  justify-content: space-between;
+  padding: 50px 200px;
+  background-color: #603c1c;
+}
+
+#navigationBar a:link, #navigationBar a:visited, #navigationBar a:active {
+  text-decoration: none;
+  color: white;
+}
+
+#navigationBar a:hover {
+  cursor: pointer;
+}
+
+#logInSignUpLinks {
+  position: relative;
+  height: 100vh;
+  width: 100%;
+  background-image: url('images/transparent background.jpg');
+  background-size: cover;
+  background-color: #FAE7C7;
+  display: flex;
+  justify-content: center;
+}
+
+#logInSignUpLinksBoxes {
+  min-width: 10%;
+  padding: 2%;
+  margin: 5% 5%;
+  background-color: #c16228;
+  font-size: 20pt;
+  height: 10%;
+  border: 2px solid #603c1c;
+  box-shadow: 5px 5px 5px;
+}
+
+#logInSignUpLinks a:link, #logInSignUpLinks a:visited, #logInSignUpLinks a:active {
+  text-decoration: none;
+  color: white;
+}
+
+#logInSignUp a:hover {
+  cursor: pointer;
+}
+
+</style>
+    
+</head>
+<body>
+    <header>
+        <div id="navigationBar">
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Pricing</a>
+            <a href="#">Testimonies</a>
+            <a href="#">Contact</a>
+        </div>
+    </header>
+
+
+
+    <div id="logInSignUpLinks">
+      <div id="logInSignUpLinksBoxes">
+        <a href="/login" id="logInSignUpLinksBoxes2">Login</a>
+      </div>
+      <div id="logInSignUpLinksBoxes">
+        <a href="/register" id="logInSignUpLinksBoxes2">Register</a>
+      </div>
+    </div>
+
+    <footer>
+
+    </footer>
 </body>
 </html>

0541b1794fc843ccddad43d6fbe4df43dd1f8b02
Taratiara <taratiara789@aol.com>
2023-12-12 09:24:30 -0500

    launch page design added

--- resources/views/Shared/homepage.blade.php
+++ resources/views/Shared/homepage.blade.php
@@ -22,22 +22,44 @@
 #navigationBar {
   display: flex;
   justify-content: space-between;
-  margin-left: 200px;
-  margin-right: 200px;
+  padding: 50px 200px;
+  background-color: #603c1c;
 }
 
 #navigationBar a:link, #navigationBar a:visited, #navigationBar a:active {
   text-decoration: none;
-  color: black;
+  color: white;
 }
 
 #navigationBar a:hover {
   cursor: pointer;
 }
 
-#logInSignUp a:link, #logInSignUp a:visited, #logInSignUp a:active {
+#logInSignUpLinks {
+  position: relative;
+  height: 100vh;
+  width: 100%;
+  background-image: url('images/transparent background.jpg');
+  background-size: cover;
+  background-color: #FAE7C7;
+  display: flex;
+  justify-content: center;
+}
+
+#logInSignUpLinksBoxes {
+  min-width: 10%;
+  padding: 2%;
+  margin: 5% 5%;
+  background-color: #c16228;
+  font-size: 20pt;
+  height: 10%;
+  border: 2px solid #603c1c;
+  box-shadow: 5px 5px 5px;
+}
+
+#logInSignUpLinks a:link, #logInSignUpLinks a:visited, #logInSignUpLinks a:active {
   text-decoration: none;
-  color: black;
+  color: white;
 }
 
 #logInSignUp a:hover {
@@ -61,11 +83,13 @@
 
 
     <div id="logInSignUpLinks">
-        <a href="/login">Login</a>
-        <a href="/register">Register</a>
+      <div id="logInSignUpLinksBoxes">
+        <a href="/login" id="logInSignUpLinksBoxes2">Login</a>
+      </div>
+      <div id="logInSignUpLinksBoxes">
+        <a href="/register" id="logInSignUpLinksBoxes2">Register</a>
+      </div>
     </div>
-
-
 
     <footer>
 
